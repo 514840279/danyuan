0d7b782c34208700bc7919f87828587cf00b1d7f
514840279 <514840279@qq.com>
2018-06-22 06:41:48 +0800

    t

---
+++ resources/dayuan-parent/danyuan-utils/src/main/java/org/danyuan/utils/zidian/CityService.java
@@ -0,0 +1,15 @@
+package org.danyuan.utils.zidian;
+
+/**    
+*  文件名 ： CityService.java  
+*  包    名 ： org.danyuan.utils.zidian  
+*  描    述 ： TODO(用一句话描述该文件做什么)  
+*  机能名称：
+*  技能ID ：
+*  作    者 ： wang  
+*  时    间 ： 2017年4月15日 下午6:53:02  
+*  版    本 ： V1.0    
+*/
+public class CityService {
+	
+}
---
+++ resources/dayuan-parent/danyuan-utils/src/main/java/org/danyuan/utils/zidian/ProvinceService.java
@@ -0,0 +1,15 @@
+package org.danyuan.utils.zidian;
+
+/**    
+*  文件名 ： ProvinceService.java  
+*  包    名 ： org.danyuan.utils.zidian  
+*  描    述 ： TODO(用一句话描述该文件做什么)  
+*  机能名称：
+*  技能ID ：
+*  作    者 ： wang  
+*  时    间 ： 2017年4月15日 下午6:52:46  
+*  版    本 ： V1.0    
+*/
+public interface ProvinceService {
+	
+}
